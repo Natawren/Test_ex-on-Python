486899827bbf28fc84d023cda1ab89f739970e9f
Marie Narbert <mnarbert@mi-i5.21-school.ru>
2019-04-12 17:39:18 +0300

    added ex02

---
+++ Data_Structures/ex2.py
@@ -0,0 +1,7 @@
+# Write code to reverse a C-Style String (C-String means that “abcd” is represented as  ve characters, including the null character )
+
+def     reverse_str(string)
+    retrun (return s[::-1])
+
+string = input()
+print(reverse_str(string))
