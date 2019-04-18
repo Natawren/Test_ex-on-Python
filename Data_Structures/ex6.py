10f09f0918948e2015eba0abd37a126ae0514c4c
Marie Narbert <mnarbert@mi-j5.21-school.ru>
2019-04-18 16:12:30 +0300

    added ex06 works, but need to be tested

--- Data_Structures/ex6.py
+++ Data_Structures/ex6.py
@@ -2,9 +2,9 @@
 import sys
 
 data = []
-
+zerros = [[],[]]
 def check_len_row(data, line):
-    if len(data) > 0 and len(data[0]) != len(line):
+    if len(data) > 0 and len(data[0]) != len(line.split()):
         print("Wrong number of ints in row\n")
         return False
     return True
@@ -22,6 +22,35 @@ def     reading_data(data):
         except ValueError:
             print("That's not an int!\n")
             return False
+    len_x = len(data)
+    if  len_x == 0:
+        return False
     return True
+
+def     check_null(data, zerros):
+    for i in range(len(data)):
+        for j in range(len(data[i])):
+            if data[i][j] == '0':
+                if (i not in zerros[0]):
+                    zerros[0].append(i)
+                if (j not in zerros[1]):
+                    zerros[1].append(j)
+    print(zerros)
+    return zerros
+
+def     set_null(data):
+    for i in zerros[0]:
+        for j in range(len(data[i])):
+            data[i][j] = 0
+    for j in zerros[1]:
+        for i in range(len(data)):
+            data[i][j] = 0
+    return data
+
 if reading_data(data) == True:
+    zerros = check_null(data, zerros)
+    data = set_null(data)
     print(data)
+else:
+    print(False)
+
