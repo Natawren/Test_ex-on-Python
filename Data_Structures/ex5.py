ae6bff25a1c4981494230ec751144d3395787d50
Marie Narbert <mnarbert@mi-i5.21-school.ru>
2019-04-19 17:03:49 +0300

    partly added 05

--- Data_Structures/ex5.py
+++ Data_Structures/ex5.py
@@ -3,6 +3,25 @@
 import sys
 
 data = []
+
+def rotate_matrix(data):
+    len_data = len(data)
+    if len_data % 2 != 0:
+        len_data -= 1
+    for i in range(len_data // 2 ):
+        last = len_data - i - 1
+        j = i
+        while j < last:
+            offset = j - i
+            temp = data[i][j]
+            data[i][j] = data[last - offset][j]
+            data[last - offset][j] = data[last][last - offset]
+            data[last][last - offset] = data[j][last]
+            data[j][last] = temp
+            j += 1
+    return data
+
+
 def check_len_row(data, line):
     if len(data) > 0 and len(data[0]) != len(line):
         print("Wrong number of ints in row\n")
@@ -30,6 +49,7 @@ def     reading_data(data):
     return True
 
 if reading_data(data) == True and check_len_col(data) == True:
+    data = rotate_matrix(data)
     print(data)
 else:
     print(False)
